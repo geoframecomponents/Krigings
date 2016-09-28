a54e239839b0cdee22258ef2cf3a5295f1603ccf
Marialaura Bancheri <maryban@hotmail.it>
2016-09-28 09:36:02 +0200

    The component TrendAnalysis has been changed in most of its parts: - Deleted the ouput of the detrended values, since it was a dpulicate of the residuals; - Delted all the info relatd to the interpolated stations, since the component is computing the trend of the input statations; - residuals set to 0 if their value is negative (this means that the initial value of the variable was zero)

--- src/trendAnalysis/TrendAnalysis.java
+++ src/trendAnalysis/TrendAnalysis.java
@@ -20,10 +20,8 @@ import static org.jgrasstools.gears.libs.modules.JGTConstants.isNovalue;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Set;
+
 
 import oms3.annotations.Author;
 import oms3.annotations.Description;
@@ -44,7 +42,6 @@ import org.jgrasstools.gears.libs.modules.JGTModel;
 import org.jgrasstools.gears.libs.modules.ModelsEngine;
 import org.jgrasstools.gears.libs.monitor.IJGTProgressMonitor;
 import org.jgrasstools.gears.libs.monitor.LogProgressMonitor;
-import org.jgrasstools.gears.utils.sorting.QuickSortAlgorithm;
 import org.jgrasstools.hortonmachine.i18n.HortonMessageHandler;
 import org.opengis.feature.simple.SimpleFeature;
 
@@ -87,21 +84,6 @@ public class TrendAnalysis extends JGTModel {
 	public HashMap<Integer, double[]> inData = null;
 
 
-	@Description("The vector of the points in which the data have to be interpolated.")
-	@In
-	public SimpleFeatureCollection inInterpolate = null;
-
-
-	@Description("The field of the interpolated vector points, defining the id.")
-	@In
-	public String fInterpolateid = null;
-
-
-	@Description("The field of the interpolated vector points, defining the elevation.")
-	@In
-	public String fPointZ = null;
-
-
 	@Description("The progress monitor.")
 	@In
 	public IJGTProgressMonitor pm = new LogProgressMonitor();
@@ -112,24 +94,12 @@ public class TrendAnalysis extends JGTModel {
 	public boolean doIncludezero = true;
 
 
-	@Description("In the case of kriging with neighbor, maxdist is the maximum distance "
-			+ "within the algorithm has to consider the stations")
-	@In
-	public double maxdist;
-
-
-	@Description("In the case of kriging with neighbor, inNumCloserStations is the number "
-			+ "of stations the algorithm has to consider")
-	@In
-	public int inNumCloserStations;
-
-
 	@Description("Switch for detrended mode.")
 	@In
 	@Out
 	public boolean doDetrended = true;
 
-	
+
 	@Description("The threshold on correlation coefficient for the trend in detrendend mode.")
 	@In
 	public double thresholdCorrelation;
@@ -138,8 +108,8 @@ public class TrendAnalysis extends JGTModel {
 	@Description("The hashmap withe the interpolated results")
 	@Out
 	public HashMap<Integer, double[]> outResiduals= null;
-	
-	
+
+
 	@Description("The hashmap withe the interpolated results")
 	@Out
 	public HashMap<Integer, double[]> outDetrendedValues= null;
@@ -151,26 +121,19 @@ public class TrendAnalysis extends JGTModel {
 
 	int id;
 
-	
+
 	@Description("The double value of the trend")
 	@Out
-	double trend;
+	double trend_intercept;
+
+	@Description("The double value of the trend")
+	@Out
+	double trend_coefficient;
 
 
-	/**
-	 * Executing ordinary kriging.
-	 * <p>
-	 * <li>Verify if the parameters are correct.
-	 * <li>Calculating the matrix of the covariance (a).
-	 * <li>For each point to interpolated, evalutate the know term vector (b)
-	 * and solve the system (a x)=b where x is the weight.
-	 * </p>
-	 *
-	 * @throws Exception the exception
-	 */
 
 	@Execute
-	public void executeKriging() throws Exception {
+	public void execute() throws Exception {
 
 		verifyInput();
 
@@ -247,15 +210,15 @@ public class TrendAnalysis extends JGTModel {
 		/*
 		 * Check if the coordinates or the values are the same for all the measurements stations.
 		 * xStationInitialSet has the dimensions of the coordinates of the measurements points 
-		 * plus 1 (the station where it is going to interpolate)
+		 * 
 		 */
 
 
-		double[] xStationInitialSet = new double[nStaz+ 1];
-		double[] yStationInitialSet = new double[nStaz+ 1];
-		double[] zStationInitialSet = new double[nStaz+ 1];
-		double[] hStationInitialSet = new double[nStaz+ 1];
-		int[] idStationInitialSet = new int[nStaz + 1];
+		double[] xStationInitialSet = new double[nStaz];
+		double[] yStationInitialSet = new double[nStaz];
+		double[] zStationInitialSet = new double[nStaz];
+		double[] hStationInitialSet = new double[nStaz];
+		int[] idStationInitialSet = new int[nStaz];
 
 		boolean areAllEquals = true;
 		if (nStaz != 0) {
@@ -295,178 +258,56 @@ public class TrendAnalysis extends JGTModel {
 			}
 		}
 
-		/* in case of kriging with neighbor computes the distances between the
-		 * point where is going to interpolate and the other stations and it
-		 * sorts them
-		 */ 
-
-		if (inNumCloserStations > 0 || maxdist>0) {
-
-			inNumCloserStations= (inNumCloserStations> nStaz)? nStaz:inNumCloserStations;	
-
-			double x2, y2;
-			double dDifX, dDifY;
-			double distanceVector[] = new double[xStationInitialSet.length];
-			double pos[] = new double[xStationInitialSet.length];
 
 
-			for (int jj = 0; jj < xStationInitialSet.length; jj++) {
 
-				x2 = xStationInitialSet[jj];
-				y2 = yStationInitialSet[jj];
-
-				dDifX = xStationInitialSet[n1] - x2;
-				dDifY = yStationInitialSet[n1] - y2;
-				distanceVector[jj] = Math.sqrt(dDifX * dDifX + dDifY * dDifY); 
-				pos[jj] = jj;					
-			}
-
-			// sorts the distances
-			QuickSortAlgorithm t = new QuickSortAlgorithm(pm);
-			t.sort(distanceVector, pos);
-
-			// posDist is the number of the stations within the distance 
-			int posDist = distanceVector.length;
-			for (int k = 0; k < distanceVector.length; k++) {
-				if (distanceVector[k] > maxdist) {
-					posDist = k;
-					break;
-				}
-			}
-
-			// in case there are no stations within the distance, the algorithm considers
-			// at least the nearest 3
-			posDist=(posDist == 1)?posDist += 3:posDist;
-
-			/*
-			 * The dimension of the new vector of the station is then defined
-			 * by the actual number of the station within the distance or defined 
-			 * by the users
-			 */
-			int dim=(inNumCloserStations > 0)?inNumCloserStations+1:posDist;
+		double[] hresiduals=null;
+		double[] zStation = zStationInitialSet;
+		double[] hStation = hStationInitialSet;			
 
 
-			double[] xStationWithNeighbour = new double[dim];
-			double[] yStationWithNeighbour = new double[dim];
-			int[] idStationWithNeighbour = new int[dim];
-			double[] zStationWithNeighbour = new double[dim];
-			double[] hWithNeighbour = new double[dim];
+
+		if (n1 != 0) {
+			if (!areAllEquals && n1 > 1) {
+
+				Regression r = new Regression();
+
+				r = new Regression(zStation, hStation);
+				r.linear();
 
 
-			// it is necessary to actualize the counter of the stations
-			n1=0;
+				/*If there is a trend for meteorological
+				 * variables and elevation and it is statistically significant 
+				 * then the residuals from this linear trend
+				 * are computed for each meteorological stations.
+				 */
 
-			for (int i = 1; i < dim; i++) {					
-				if (doIncludezero) {
-					if (Math.abs(hStationInitialSet[(int) pos[i]]) >= 0.0) { // TOLL
-						xStationWithNeighbour[n1] = xStationInitialSet[(int) pos[i]];
-						yStationWithNeighbour[n1] = yStationInitialSet[(int) pos[i]];
-						zStationWithNeighbour[n1] = zStationInitialSet[(int) pos[i]];
-						idStationWithNeighbour[n1] = idStationInitialSet[(int) pos[i]];
+				if (r.getXYcorrCoeff() > thresholdCorrelation) {
 
-						hWithNeighbour[n1] = hStationInitialSet[(int) pos[i]];
-						n1 += 1;
-					}
+					trend_intercept=r.getBestEstimates()[0];
+					trend_coefficient=r.getBestEstimates()[1];
+					hresiduals = r.getResiduals();
+
 				} else {
-					if (Math.abs(hStationInitialSet[(int) pos[i]]) > 0.0) {
-						xStationWithNeighbour[n1] = xStationInitialSet[(int) pos[i]];
-						yStationWithNeighbour[n1] = yStationInitialSet[(int) pos[i]];
-						zStationWithNeighbour[n1] = zStationInitialSet[(int) pos[i]];
-						idStationWithNeighbour[n1] = idStationInitialSet[(int) pos[i]];
-
-						hWithNeighbour[n1] = hStationInitialSet[(int) pos[i]];
-						n1 += 1;
-
-					}
+					System.out.println("The trend is not significant");
+					doDetrended=false;
+					
 				}
-
 			}
 
-			xStationInitialSet = xStationWithNeighbour;
-			yStationInitialSet = yStationWithNeighbour;
-			zStationInitialSet = zStationWithNeighbour;
-			idStationInitialSet = idStationWithNeighbour;
-			hStationInitialSet = hWithNeighbour;
+
+		}else{
+			pm.errorMessage("No value for this time step");
 
 		}
 
+		storeResult(hresiduals,hStation,idStationInitialSet);
 
-		LinkedHashMap<Integer, Coordinate> pointsToInterpolateId2Coordinates = null;
-
-		int numPointToInterpolate = 0;
-
-
-		pointsToInterpolateId2Coordinates = getCoordinate(numPointToInterpolate, inInterpolate, fInterpolateid);
-
-
-		Set<Integer> pointsToInterpolateIdSet = pointsToInterpolateId2Coordinates.keySet();
-		
-		Iterator<Integer> idIterator = pointsToInterpolateIdSet.iterator();
-
-		int j = 0;
-
-		double[]  hsokres = null;
-		double[] hStation=null;
-		int[] idArray = new int[pointsToInterpolateId2Coordinates.size()];
-
-		while (idIterator.hasNext()) {
-			n1 = xStationInitialSet.length - 1;
-
-			id = idIterator.next();
-			idArray[j] = id;
-
-			Coordinate coordinate = (Coordinate) pointsToInterpolateId2Coordinates.get(id);
-
-
-			zStationInitialSet[n1] = coordinate.z;
-
-			double[] zStation = zStationInitialSet;
-			hStation = hStationInitialSet;			
-
-
-			if (n1 != 0) {
-
-				if (!areAllEquals && n1 > 1) {
-					pm.beginTask(msg.message("analysis.working"),
-							pointsToInterpolateId2Coordinates.size());
-
-
-					Regression r = new Regression();
-					r = new Regression(zStation, hStation);
-					r.linear();
-					trend = r.getBestEstimates()[0] + coordinate.z
-							* r.getBestEstimates()[1];
-
-					hsokres = r.getResiduals();
-
-					/*If there is a trend for meteorological
-					 * variables and elevation and it is statistically significant 
-					 * then the residuals from this linear trend
-					 * are computed for each meteorological stations.
-					 */
-
-					if (r.getXYcorrCoeff() > thresholdCorrelation && trend > 0) {
-
-						for (int i = 0; i < hStation.length - 1; i++) {
-
-							double t = r.getBestEstimates()[0]
-									+ zStation[i] * r.getBestEstimates()[1];
-							hStation[i] = hStation[i] - t;
-
-						}
-					} else {
-						System.out.println("The trend is not significant");
-						doDetrended=false;
-					}
-
-				}
-
-			}
-
-		}
-		
-		storeResult(hsokres,hStation, idArray);
 	}
+
+
+
+
 
 
 
@@ -499,66 +340,26 @@ public class TrendAnalysis extends JGTModel {
 	}
 
 
-	/**
-	 * Extract the coordinate of a FeatureCollection in a HashMap with an ID as
-	 * a key.
-	 *
-	 * @param nStaz the number of the stations
-	 * @param collection is the collection of the considered points 
-	 * @param idField the field containing the id of the stations 
-	 * @return the coordinate of the station
-	 * @throws Exception if a field of elevation isn't the same of the collection
-	 */
-	private LinkedHashMap<Integer, Coordinate> getCoordinate(int nStaz,
-			SimpleFeatureCollection collection, String idField)
-					throws Exception {
-		LinkedHashMap<Integer, Coordinate> id2CoordinatesMcovarianceMatrix = new LinkedHashMap<Integer, Coordinate>();
-		FeatureIterator<SimpleFeature> iterator = collection.features();
-		Coordinate coordinate = null;
-		try {
-			while (iterator.hasNext()) {
-				SimpleFeature feature = iterator.next();
-				int name = ((Number) feature.getAttribute(idField)).intValue();
-				coordinate = ((Geometry) feature.getDefaultGeometry())
-						.getCentroid().getCoordinate();
-				double z = 0;
-				if (fPointZ != null) {
-					try {
-						z = ((Number) feature.getAttribute(fPointZ))
-								.doubleValue();
-					} catch (NullPointerException e) {
-						pm.errorMessage(msg.message("kriging.noPointZ"));
-						throw new Exception(msg.message("kriging.noPointZ"));
-					}
-				}
-				coordinate.z = z;
-				id2CoordinatesMcovarianceMatrix.put(name, coordinate);
-			}
-		} finally {
-			iterator.close();
-		}
-
-		return id2CoordinatesMcovarianceMatrix;
-	}
-
-
-
 
 	/**
 	 * Store the result in a HashMap
 	 *
-	 * @param hsokres the vector with the residuals
+	 * @param hresiduals the vector with the residuals
 	 * @param hStation the vector with the values of the stations
 	 * @param id the associated id of the calculating points.
 	 * @throws SchemaException the schema exception
 	 */
-	private void storeResult(double [] hsokres, double [] hStation, int [] id ) throws SchemaException {
+	private void storeResult(double [] hresiduals, double [] hStation, int [] id ) throws SchemaException {
 		outResiduals = new HashMap<Integer, double[]>();
 		outDetrendedValues = new HashMap<Integer, double[]>();
-		for (int i = 0; i < hsokres.length; i++) {
-			outResiduals.put(id[i], new double[] { hsokres[i] });
+		for (int i = 0; i < hresiduals.length; i++) {			
+			hresiduals[i]=(hresiduals[i]<0)?0:hresiduals[i];
+			outResiduals.put(id[i], new double[] { hresiduals[i] });
 			outDetrendedValues.put(id[i], new double[] { hStation[i] });
 		}
 	}
+
+
+
 
 }
