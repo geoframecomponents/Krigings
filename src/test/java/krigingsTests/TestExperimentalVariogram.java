6f2a653e8c90ae2de4492f75f49963a426e0b220
Marialaura Bancheri <maryban@hotmail.it>
2017-02-03 14:09:06 +0100

    Small errro in the id field of the test, all the tests done again

--- src/test/java/krigingsTests/TestExperimentalVariogram.java
+++ src/test/java/krigingsTests/TestExperimentalVariogram.java
@@ -41,7 +41,7 @@ public class TestExperimentalVariogram{
 
 		//
 
-		String stationIdField = "id";
+		String stationIdField = "Id";
 
 		OmsShapefileFeatureReader stationsReader = new OmsShapefileFeatureReader();
 		stationsReader.file = "resources/Input/experimentalVGM/jura.shp";
